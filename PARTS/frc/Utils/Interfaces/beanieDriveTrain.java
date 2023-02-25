f0f73e15d6e0cb37f5f838148d535f3cad69e6d1
Cameron <ckiw23@gmail.com>
2023-02-24 20:57:10 -0500

    added the ability to rotate the gyros z mount

--- PARTS/frc/Utils/Interfaces/beanieDriveTrain.java
+++ PARTS/frc/Utils/Interfaces/beanieDriveTrain.java
@@ -21,86 +21,92 @@ public abstract class beanieDriveTrain extends SubsystemBase {
     AHRS gyro;
     protected DifferentialDrive mDrive;
     protected MotorControllerGroup leftControllerGroup, rightControllerGroup;
+    protected Rotation2d gyroZrotation;
 
-
-    public beanieDriveTrain(AHRS gyro, MotorControllerGroup leftControllerGroup, MotorControllerGroup rightMotorControllerGroup){
+    public beanieDriveTrain(AHRS gyro, MotorControllerGroup leftControllerGroup,
+            MotorControllerGroup rightMotorControllerGroup) {
         this.gyro = gyro;
         Shuffleboard.getTab("Debug").add(gyro);
-        
+
         this.leftControllerGroup = leftControllerGroup;
         this.rightControllerGroup = rightMotorControllerGroup;
         leftControllerGroup.setInverted(true);
         mDrive = new DifferentialDrive(leftControllerGroup, rightControllerGroup);
+        this.gyroZrotation = new Rotation2d(0);
+    }
+
+    public beanieDriveTrain(AHRS gyro, Rotation2d gryoZRotation, MotorControllerGroup leftControllerGroup,
+            MotorControllerGroup rightMotorControllerGroup) {
+        this.gyro = gyro;
+        Shuffleboard.getTab("Debug").add(gyro);
+
+        this.leftControllerGroup = leftControllerGroup;
+        this.rightControllerGroup = rightMotorControllerGroup;
+        leftControllerGroup.setInverted(true);
+        mDrive = new DifferentialDrive(leftControllerGroup, rightControllerGroup);
+        this.gyroZrotation = gryoZRotation;
     }
 
     /**
      * @apiNote only use for auto these inputs are not squared
-     * @param left left percent output
-     * @param right right percent output 
+     * @param left  left percent output
+     * @param right right percent output
      */
-    public  void move(double left, double right){
+    public void move(double left, double right) {
         mDrive.tankDrive(left, right, false);
     }
 
-    public double getVelocityGyro(){
+    public double getVelocityGyro() {
         return gyro.getVelocityZ();
     }
 
-    public void moveVolts(double leftVoltage, double rightVoltage){
-        leftControllerGroup.setVoltage(-leftVoltage); // maybe need to denegate.
+    public void moveVolts(double leftVoltage, double rightVoltage) {
+        leftControllerGroup.setVoltage(-leftVoltage); // TODO: maybe need to denegate.
         rightControllerGroup.setVoltage(rightVoltage);
         mDrive.feed();
-        
+
     }
-    public double getPitch(){
+
+    public double getPitch() {
         return -gyro.getPitch();
     }
-    
 
-
-
-    
-    public void moveArcade(double X, double rotation){
+    public void moveArcade(double X, double rotation) {
         mDrive.arcadeDrive(X, rotation);
     }
 
     public abstract Pose2d currentPose();
 
-         
-    
-
-    public  double getAngle(){
+    public double getAngle() {
         return gyro.getAngle();
     }
 
-    public double getYaw(){
+    public double getYaw() {
         return gyro.getYaw();
     }
 
-    public double getRoll(){
+    public double getRoll() {
         return -gyro.getRoll();
     }
 
-    public double getTurningRate(){
+    public double getTurningRate() {
         return gyro.getRate();
     }
 
-    public Rotation2d getRotation(){
-        return gyro.getRotation2d().times(-1).rotateBy(new Rotation2d(Math.toRadians(180)));
+    public Rotation2d getRotation() {
+        return gyro.getRotation2d().times(-1).rotateBy(gyroZrotation);
     }
 
     public abstract double leftDistance();
 
-    public abstract double rightDistance();    
+    public abstract double rightDistance();
 
-    public void stop(){
+    public void stop() {
         mDrive.tankDrive(0, 0);
     }
 
     public abstract void autonomousSetup();
 
     public abstract void teleopSetup();
-
-
 
 }
